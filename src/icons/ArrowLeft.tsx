83ab55fc706b416090086305b2230eb4e3bd3636
Alex Beckwith <alfonz129@gmail.com>
2020-03-31 10:29:20 -0600

    remove redundant prop

--- src/icons/ArrowLeft.tsx
+++ src/icons/ArrowLeft.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
 import Svg, { Polygon, SvgProps, G } from 'react-native-svg'
 
-const ArrowLeft: React.FC<SvgProps & { rotation?: number }> = props => (
+const ArrowLeft: React.FC<SvgProps> = ({ rotation, ...props}) => (
   <Svg {...props}>
-    <G rotation={props.rotation} originX={12} originY={12}>
+    <G rotation={rotation} originX={12} originY={12}>
       <Polygon points="20 11 7.8 11 13.4 5.4 12 4 4 12 12 20 13.4 18.6 7.8 13 20 13" />
     </G>
   </Svg>
